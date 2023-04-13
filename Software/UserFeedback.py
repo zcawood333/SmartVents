2702ad3c28f17f69c0397a390ee118818e78314e
mohqum <69472506+mohqum@users.noreply.github.com>
2023-04-13 14:48:51 -0400

    Mistake

--- Software/UserFeedback.py
+++ Software/UserFeedback.py
@@ -5,18 +5,30 @@ import time
 class MyClass:
     def __init__(self):
         self.timestamps_list = []
-        self.start_index = 0
-        self.target_index = -1
+       
+
+
 
       
 
-    def User_Alerts(self, timestamps_list : list):
+    def User_Alerts(self, timestamps_list : list, target_temp : float):
          self.timestamps_list = timestamps_list
          print(self.timestamps_list)
+         for x in self.timestamps_list:
+            print(x.unix_time,x.louverPos,x.temperature,x.motion)
+
+         self.initial_value = self.timestamps_list[-1].temperature
+         self.latest_value = self.timestamps_list[0].temperature
+         print("Initial value is ", self.initial_value, "and the latest value is ", self.latest_value)
+        
+         self.target_temp = target_temp
+         print("target temperature is ",self.target_temp)
+         
+
+        
 
                   
-         for x in self.timestamps_list:
-             print(x.unix_time,x.louverPos,x.temperature,x.motion)
+        
             
          
 
@@ -34,13 +46,14 @@ if __name__ == "__main__":
     my_timestamp_0 = Timestamp(120, 75, True)
 
     my_run = Run(75)
-    my_class = MyClass()
+   
 
     my_run.createTimestamp(my_timestamp_000)
     my_run.createTimestamp(my_timestamp_00)
     my_run.createTimestamp(my_timestamp_01)
     my_run.createTimestamp(my_timestamp_0)
+    my_class = MyClass()
 
-    my_class.User_Alerts(my_run.timestamps)
+    my_class.User_Alerts(my_run.timestamps, my_run.target)
 
 

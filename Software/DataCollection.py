3304578ba5c25269b4008e306e3ea0325908f967
Zach Cawood <zcawood@purdue.edu>
2023-04-16 18:56:48 -0400

    updated to allow for easier parsing of output files

--- Software/DataCollection.py
+++ Software/DataCollection.py
@@ -6,11 +6,17 @@ DATA_FOLDER_PATH = os.path.join(os.path.dirname(__file__), "../Data")
 DATE_FOLDER_PATH = os.path.join(DATA_FOLDER_PATH, dt.datetime.now().date().isoformat())
 TIME_FOLDER_PATH = os.path.join(DATE_FOLDER_PATH, dt.datetime.now().strftime("%H-%M-%S"))
 
+def dataFileName(ventUUID: int):
+    return f"{ventUUID}_data.csv"
+
+def parameterFileName(ventUUID: int):
+    return f"{ventUUID}_params.csv"
+
 def ventDataPath(ventUUID: int):
-    return os.path.join(TIME_FOLDER_PATH, f"{ventUUID}_data.csv")
+    return os.path.join(TIME_FOLDER_PATH, dataFileName(ventUUID))
 
 def ventParameterPath(ventUUID: int):
-    return os.path.join(TIME_FOLDER_PATH, f"{ventUUID}_params.csv")
+    return os.path.join(TIME_FOLDER_PATH, parameterFileName(ventUUID))
 
 def initDataCollection(vents: list):
     if not os.path.isdir(DATA_FOLDER_PATH):
@@ -24,15 +30,15 @@ def initDataCollection(vents: list):
         ventParameterFilePath = ventParameterPath(vent.id)
         if not os.path.isfile(os.path.join(TIME_FOLDER_PATH, ventDataFilePath)):
             with open(ventDataFilePath, "w") as file:
-                file.write("Timestamp, Target Temperature, Measured Temperature, LouverPosition, Motion\n")
+                file.write("Timestamp | Target Temperature | Measured Temperature | LouverPosition | Motion\n")
         if not os.path.isfile(os.path.join(TIME_FOLDER_PATH, ventParameterFilePath)):
             with open(ventParameterFilePath, "w") as file:
-                file.write("Timestamp, Master Vent, Local Control, Heat Constant, Heat Coefficients, Self Heat Coefficient Index\n")
+                file.write("Timestamp | Master Vent | Local Control | Heat Constant | Heat Coefficients | Self Heat Coefficient Index\n")
 
 def writeData(ventUUID: int, targetTemp: float, measuredTemp: float, louverPosition: float, motion: bool):
     with open(ventDataPath(ventUUID), "a") as file:
-        file.write(f"{dt.datetime.now().isoformat()}, {targetTemp}, {measuredTemp}, {louverPosition}, {motion}\n")
+        file.write(f"{dt.datetime.now().isoformat()} | {targetTemp} | {measuredTemp} | {louverPosition} | {motion}\n")
 
 def writeVentParams(ventUUID: int, master: bool, localControl: bool, heatConstant: float, heatCoeffs: np.matrix, selfHeatCoeffIndex: int):
     with open(ventParameterPath(ventUUID), "a") as file:
-        file.write(f"{dt.datetime.now().isoformat()}, {master}, {localControl}, {heatConstant}, {heatCoeffs.tolist()}, {selfHeatCoeffIndex}\n")
+        file.write(f'{dt.datetime.now().isoformat()} | {master} | {localControl} | {heatConstant} | "{heatCoeffs.flatten().tolist()[0]}" | {selfHeatCoeffIndex}\n')
